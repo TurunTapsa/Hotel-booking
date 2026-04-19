717d7c92559490469672f222afb9eda705058d1f
TurunTapsa <jasper.tapio@edu.turkuamk.fi>
2026-04-19 15:33:12 +0300

    initial commit

---
+++ hotel_booking_system.py
@@ -0,0 +1,43 @@
+#Hotel booking system
+
+
+#Data Storage here
+#Each room is stored as a dictionary with type, price, availability, and guest name
+
+
+
+# Add a room function here
+
+
+
+#View available rooms here
+
+
+
+
+#Book a room here
+
+
+
+
+
+#Check out a customer here
+
+
+
+
+
+
+#Remove a room here
+
+
+
+
+
+
+#Main menu
+
+
+
+
+
