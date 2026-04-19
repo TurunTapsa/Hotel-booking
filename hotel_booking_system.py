427c5fe8488b843238a80d8b5500f7445e09c18b
Eetu Korpi <eetu.korpi@gmail.com>
2026-04-19 16:20:57 +0300

    lisätty huoneiden tallennusmuoto

--- hotel_booking_system.py
+++ hotel_booking_system.py
@@ -2,8 +2,14 @@
 
 
 #Data Storage here
-#Each room is stored as a dictionary with type, price, availability, and guest name
-
+#Each room is stored as a dictionary with room type, price, availability, and guest name
+rooms = {
+    101: {"type": "single",  "price": 60,  "available": True, "guest": None},
+    102: {"type": "single",  "price": 60,  "available": True, "guest": None},
+    103: {"type": "double",  "price": 90,  "available": True, "guest": None},
+    104: {"type": "double",  "price": 90,  "available": True, "guest": None},
+    105: {"type": "suite",   "price": 150, "available": True, "guest": None},
+}
 
 
 # Add a room function here
